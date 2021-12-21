1d1fb15de3f376f9d56eff3c85acdce6b36007bc
Christian <christianbear1221@gmail.com>
2021-12-20 22:01:48 -0800

    ideagit add *! JSON as file data stream, to be converted to String from Obj, vise versa

--- JSON.py
+++ JSON.py
@@ -2,3 +2,18 @@
 # TODO - make it into a string
 
 # TODO - put the strinbg back into object data
+# =================
+
+
+
+# take an array (list?) - make into string
+# put all the variable data -> to a file
+# make that file into string, and then bring it back
+meString = ""
+def 
+
+# =================
+# TODO - take an object from Python
+# TODO - make it into a string
+
+# TODO - put the strinbg back into object data
