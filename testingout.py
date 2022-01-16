3d2e3b0b6ca794e38b6b6e9946aee7d1e916540b
pixman92 <GOtrustD@verizon.net>
2022-01-15 19:46:32 -0800

    ...

--- testingout.py
+++ testingout.py
@@ -3,6 +3,7 @@
 def run():
     tmp = [[['name?', 'sam'], ['time', 'noon'],['lucky', 'yup'] ], ['people', 'are awesome'], [['age', '12'], ['place', 'nowhere']]]
 
+    tmp=str(tmp)
     # while(tmp.find(']]')
     holdingArray = []
     for x in range (0, len(tmp)):
